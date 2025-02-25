4312ee6f5ec25d4b0d5a9fb8d057f4475cccd311
DavinaWorld <kdbj12003@gmail.com>
2025-02-25 19:59:36 +0900

    이전 작업 코드 제거

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -23,32 +23,3 @@ export default function TaskList({ status }: { status: ICard }) {
     </li>
   );
 }
-
-// export default class TaskList {
-//   status: ICard;
-//   constructor(status: ICard) {
-//     this.status = status;
-//     console.log(status);
-//   }
-
-//   render(): string {
-//     return `
-//       <li>
-//         <article class="task-card ${this.status.priority.toLowerCase()}">
-//             <header class="task-card-header">
-//                 <span class="date-wrapper">
-//                     <img src=${date} alt="date">
-//                     <time>${this.status.startDate}</time>
-//                     ${this.status.endDate || ''}
-//                 </span>
-//                 <span class="priority">${this.status.priority} priority</span>
-//             </header>
-//             <div class="task-contents">
-//                 <h3>${this.status.title}</h3>
-//                 <p>${this.status.description}</p>
-//             </div>
-//         </article>
-//       </li>
-//     `;
-//   }
-// }
